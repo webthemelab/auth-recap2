23ab8059a76127b692db420633ad521a4abfb3c4
webthemelab <75390272+webthemelab@users.noreply.github.com>
2021-03-15 18:41:10 +0600

    facebook firebase add

--- src/App.js
+++ src/App.js
@@ -4,34 +4,65 @@ import "firebase/auth";
 import firebaseConfig from './firebase.config';
 import { useState } from 'react';
 
-firebase.initializeApp(firebaseConfig);
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}else {
+  firebase.app(); // if already initialized, use that one
+}
 
 
 function App() {
   const [user, setUser] = useState({})
-  const provider = new firebase.auth.GoogleAuthProvider();
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
   const handleGoogleSignIn = () => {
     firebase.auth()
-      .signInWithPopup(provider)
+      .signInWithPopup(googleProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        console.log(user);
+        setUser(user);
+      }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential)
+      });
+  }
+  const handleFacebookSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(fbProvider)
       .then((result) => {
          var credential = result.credential;
-         var token = credential.accessToken;
+         
          var user = result.user;
-         console.log(user);
+         console.log('fb user', user);
+
+         var accessToken = credential.accessToken;
          setUser(user);
-       }).catch((error) => {
+
+       })
+      .catch((error) => {
          var errorCode = error.code;
-        var errorMessage = error.message;
+         var errorMessage = error.message;
          var email = error.email;
-        var credential = error.credential;
-        console.log(errorCode, errorMessage, email , credential)
-      });
+         var credential = error.credential;
+        console.log(errorMessage, errorCode, email, credential)
+       });
   }
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Sign in using google</button>
+      <br />
+      <br />
+      <button onClick={handleFacebookSignIn}>Sign in using Facebook</button>
       <h3>Email: {user.email}</h3>
-      <img src={user.photoURL} alt=""/>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
